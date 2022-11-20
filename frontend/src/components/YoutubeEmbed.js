10bd287e7d2e2c8f01bfccb84630007fdf013124
Bennett Gunson <bengunson@gmail.com>
2022-11-19 17:33:24 -0700

    add dashboard page with suggested videos from youtube api

---
+++ frontend/src/components/YoutubeEmbed.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const YoutubeEmbed = ({ embedId }) => (
+    <div className="video-responsive">
+      <iframe
+        width="240"
+        height="130"
+        src={`https://www.youtube.com/embed/${embedId}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+);
+
+export default YoutubeEmbed;
