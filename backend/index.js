a97a9c0b2728c2ed24963c75fdf906d6d587ce9b
Bennett Gunson <bengunson@gmail.com>
2022-11-19 18:48:05 -0700

    add initial backend code

---
+++ backend/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server, Socket } = require("socket.io");
+const io = new Server(server);
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend','build', 'index.html'));
+});
+
+
+const users = {};
+const chatLog = [];
+
+io.on("connection", (socket) => {
+    console.log("a client connected");
+
+    socket.on("join chat", (user) => {
+        users[user.id] = {
+            name: user.profile.data.email,
+            id: user.id
+        };
+        let msg = {
+            content: `${users[user.id].name.split("@")[0]} has joined the chat.`,
+            user: {
+                id: null,
+                name: "SERVER"
+            },
+            time: Date.now()
+        }
+        io.emit("chat message", msg);
+        chatLog.forEach(m => socket.emit("chat message", m));
+    });
+
+    socket.on("chat message", (data) => {
+        let msg = {
+            time: Date.now(),
+            user: users[data.user.id],
+            content: data.content
+        }
+        io.emit("chat message", msg);
+        chatLog.push(msg);
+    });
+
+    socket.on("leave chat", (user) => {
+        let msg = {
+            content: `${users[user.id].name.split("@")[0]} has left the chat.`,
+            user: {
+                id: null,
+                name: "SERVER"
+            },
+            time: Date.now()
+        }
+        io.emit("chat message", msg);
+        delete users[user.id];
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
